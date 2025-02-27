46d1a22788dd3656db5351e6c99554d32fa3afa7
oktab <aslbekxayrullayev1@gmail.com>
2025-02-27 08:32:22 +0300

    count

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import './Components/Body/Body'
 
 function App() {
-
+  const [count, setCount] = useState(0);
   return (
     <>
-    <h1>Aslbek</h1>
+      <div className="flex items-center gap-[20px]">
+        <button onClick={() => setCount(count => count + 1)} className='text-2xl bg-gray-600 w-[50px] h-[50px]'>+</button>
+        <h1 id='text' className='text-2xl text-gray-700'>{count}</h1>
+        <button onClick={() => setCount(count => count - 1)} className='text-2xl bg-gray-600 w-[50px] h-[50px]'>-</button>
+      </div >
     </>
   )
 }
