a0ab2a9153d616961bea7b8547aa41e17d1f64b7
Stilla <sunnatilla95@gmail.com>
2025-02-27 10:32:11 +0500

    coun ter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from './Components/Navigation/Nav'
+import Header from './Components/Header/Header'
 
 function App() {
 
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <h1>Stilla</h1>
+      <header>
+        <div className="container">
+          <button onClick={() => setCount(prev => prev - 1)}>-</button>
+          <h1>{count}</h1>
+          <button onClick={() => setCount(prev => prev + 1)}>+</button>
+          <Nav />
+          <Header />
+        </div>
+      </header>
     </>
   )
 }
